c75d5504ef04f7c797737859ae32cf8a2a1da8d4
kangthef00 <victini20111@gmail.com>
2023-04-11 00:06:07 +0900

    Assignment 2

---
+++ nextjs-blog/pages/posts/first-page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h1>I'm Kang Seokwon. This is me.</h1><ol>
+	<li>I love rock and roll, blues, jazz.</li>
+	<li>I have a dog. His name is ZEE or ZZI.He is Maltese.</li>
+    <li>I play guitar.</li>
+    <li>I love watching moives in theatre.When i was in highschool 2nd year, i watched more than 100 movies in theatre.</li>
+    <li>My dream is to join NINTENDO CO.</li></ol>
+            <Link href="/">Back to home</Link> //GO TO "/"(MAIN PAGE)
+        </div>
+    )
+}
